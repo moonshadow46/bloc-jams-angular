dbc7f631bd826ea0f426620fa5334c1787a13f01
Josh <jiac.lee46@gmail.com>
2017-08-15 02:17:49 -0700

    A3 Assignment 1st try

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,7 +13,11 @@
       })
       .state('album', {
         url: '/album',
-        teamplateUrl: '/templates/album.html'
+        templateUrl: '/templates/album.html'
+      })
+      .state('collection', {
+        url: '/collection',
+        templateUrl: '/templates/collection.html'
       });
   }
 
