901fa6ee4d703aad3af907787e2de3e53526e55b
Josh <jiac.lee46@gmail.com>
2017-09-02 23:56:52 -0700

    Checkpoint A9 1st Try

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -111,7 +111,7 @@
       var currentSongIndex = getSongIndex(SongPlayer.currentSong);
       currentSongIndex++;
 
-      if (currentSongIndex === currentAlbum.songs.length) {
+      if (currentSongIndex > currentAlbum.songs.length) {
         stopSong(SongPlayer.currentSong);
       } else {
         var song = currentAlbum.songs[currentSongIndex];
